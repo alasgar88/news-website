612c7790a04d83021c3cc2aa28a84a665951f17f
alasgar88 <mahmudov.alasgar@gmail.com>
2022-08-18 03:11:39 +0400

    terminal error1

--- src/pages/Detail.js
+++ src/pages/Detail.js
@@ -1,7 +1,5 @@
 import React from "react";
 import { Header, SingleNewsDetail } from "../components";
-import { Loading } from "../components";
-import { useSelector } from "react-redux";
 
 const Detail = () => {
   return (
