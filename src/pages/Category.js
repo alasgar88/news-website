8f7e7f44e61a9f3c651366a13b6f34e8ece2b9bd
alasgar88 <85016138+alasgar88@users.noreply.github.com>
2022-08-19 11:41:17 +0400

    Update Category.js

--- src/pages/Category.js
+++ src/pages/Category.js
@@ -4,12 +4,15 @@ import { useParams } from "react-router-dom";
 import { Loading, NewsList, Header } from "../components";
 import { getData } from "../features/news/newsSlice";
 import { setCategoryName } from "../features/category/categorySlice";
+import { Error } from "../pages";
+import categoryList from "../utils/category";
 
 const Category = () => {
   const { isLoading } = useSelector((store) => store.news);
   const { categoryName } = useSelector((store) => store.category);
   const { category } = useParams();
   const dispatch = useDispatch();
+  const condition = categoryList.some((item) => item.name === category);
 
   useEffect(() => {
     dispatch(getData(category));
@@ -17,6 +20,9 @@ const Category = () => {
   }, [category, categoryName, dispatch]);
   if (isLoading) {
     return <Loading />;
+  }
+  if (!condition) {
+    return <Error />;
   }
   return (
     <div className='landing'>
